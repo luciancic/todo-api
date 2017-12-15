14807d370c2eea873757d31b6acbb3d17459ea4e
lucian95 <luciancondrea95@gmail.com>
2017-12-14 19:05:23 -0500

    Changed mongo uri to f fit heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect (mongoUri, {
   useMongoClient: true
 });
 
