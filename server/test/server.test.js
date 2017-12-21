7563923dd6e3e36a8902850ed69b5df72673def1
lucian95 <luciancondrea95@gmail.com>
2017-12-21 17:37:39 -0500

    Add GET /users and /users/me test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,20 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, users, populateTodos, populateUsers} = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-  }, {
-    _id: new ObjectID(),
-    text: 'Second test todo'
-  }];
-
-beforeEach((done) => {
-  Todo.remove({})
-  .then(() => Todo.insertMany(todos))
-  .then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -168,5 +159,49 @@ describe('PATCH /todos/:id', () => {
       .patch(`/todos/123}`)
       .expect(400)
       .end(done);
+  });
+});
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', '123456')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('GET /users', () => {
+  it('should get all users', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res).toExist();
+        expect(res.body.length).toBe(2);
+        expect(res.body[0].email).toExist();
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+  it('should post a new user' (done) => {
+    done();
   });
 });
