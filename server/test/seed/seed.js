ce65e72e2277762e504480add4c64bfd4e6df583
lucian95 <luciancondrea95@gmail.com>
2017-12-21 17:38:28 -0500

    Refactor seed data out of test file

---
+++ server/test/seed/seed.js
@@ -0,0 +1,61 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+  }, {
+    _id: new ObjectID(),
+    text: 'Second test todo'
+  }];
+
+let id = new ObjectID();
+const users = [
+  {
+    _id: id,
+    email: 'andrew@example.com',
+    password: 'userOnePass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({
+        _id: id,
+        access: 'auth'
+      }, 'secretSalt')
+      .toString()
+    }]
+  },
+  {
+    _id: new ObjectID(),
+    email: 'lucian@example.com',
+    password: 'userTwoPass',
+    tokens: [{
+      access: 'auth',
+      token: 'abc123'
+    }]
+  }
+];
+
+const populateTodos = (done) => {
+  Todo.remove({})
+  .then(() => Todo.insertMany(todos))
+  .then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    let userOne = new User(users[0]).save();
+    let userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+module.exports = {
+  todos,
+  users,
+  populateTodos,
+  populateUsers
+};
