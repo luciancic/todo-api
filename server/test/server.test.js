37c1390ed730d1b98bfabf609f9b39dd39b7044f
lucian95 <luciancondrea95@gmail.com>
2017-12-27 18:19:35 -0500

    Add DELETE /users/me/token test case

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -325,3 +325,27 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+//
+// DELETE /users/me/token
+//
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
