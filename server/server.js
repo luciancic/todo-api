41fc47fd087075779954c4c568db08c90121a15b
lucian95 <luciancondrea95@gmail.com>
2017-12-27 18:07:07 -0500

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -125,6 +125,14 @@ app.delete('/todos/:id', (req, res) => {
 	});
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
+	})
+});
+
 
 //
 // PATCH routes
