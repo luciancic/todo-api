e0eafa323c15429334a46eba099ab8e7e35cd260
Lucian Condrea <luciancondreadev@gmail.com>
2019-07-12 13:25:29 -0400

    update test and dev db uri

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
 	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+	process.env.MONGODB_URI = require('./dbCredentials').MONGODB_URI;
 } else if (env === 'test') {
 	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+	process.env.MONGODB_URI = require('./dbCredentials').MONGODB_URI;
 }
